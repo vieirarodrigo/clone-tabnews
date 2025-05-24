941867450fad11f3251c966a80dfa9d6d7a64e9e
vieirarodrigo <vieirarodrigohenrique@gmail.com>
2025-05-24 00:28:06 -0300

    Criado o pages, index e o meu oi para o meu amor

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <>
+        <h1>Oi amor o/</h1>
+        <p>Esse é um dos meus primeiros passo, graças a você. Saiba que eu te amo, eu vou sempre batalhar pela nossa família. Espero estar sendo um bom marido e um bom pai. Quero muito sempre estar com vocês.</p>
+        <p>Amo você, amo o Ravi. Para iniciar essa jornada deixo aqui meu oi, pela internet!</p>
+        </>
+    )
+}
+
+export default Home;
