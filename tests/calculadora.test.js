28045e6ff903ddae75c5dca714557b8971ad06a6
vieirarodrigo <vieirarodrigohenrique@gmail.com>
2025-06-17 23:30:11 -0300

    Criar um "Teste de Verdade"

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,19 @@
-test("Espero que 1 seja 1", () => {
-  expect(2).toBe(1);
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+
+  expect(resultado).toBe(4);
+});
+
+test("somar 5 + 100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+
+  expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100 deveria retornar 'Erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+
+  expect(resultado).toBe("Erro");
 });
