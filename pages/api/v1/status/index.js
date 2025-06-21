d65411fc33ca4d8c90f5b6d7d6e7a2ab64a0a892
vieirarodrigo <vieirarodrigohenrique@gmail.com>
2025-06-20 23:25:25 -0300

    Adicionado `.env` e `database.js`. Finalizado dia 18

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   response.status(200).json({ chave: "API OK Coração" });
+
+  const result = await database.query("SELECT 1 + 1 as sum;");
+
+  console.log(result.rows);
 }
 
 export default status;
