56d6c297ea9946159eada5bbaadd76d8c7d66671
Rodrigo Henrique Vieira <vieirarodrigohenrique@gmail.com>
2026-01-05 10:54:02 -0300

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function  Home() {
+    return <h1>
+        Rapha,
+
+        Obrigado por ser a minha esposa, te amo muito!
+        Espero que Deus continue nos abençoando e nos guiando em nossa jornada juntos.
+
+        Se me ama da uma inicia uma dança maluca :)
+    </h1>
+}
+
+export default Home
